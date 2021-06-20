82d3923ebd21d317e1f8744c285beb3cfd9ba555
Ivan Novyshev <I-N-Zone@yandex.ru>
2021-06-20 16:27:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
